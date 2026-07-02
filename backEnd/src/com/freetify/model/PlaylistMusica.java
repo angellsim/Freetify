d2c3229531e3fc74d48372360ceb3f40570d8a6f
raiden <alicewolffernandes@gmail.com>
2026-07-01 22:59:23 -0300

    add: adicionada classe com fk de musica e playlist

--- backEnd/src/com/freetify/model/PlaylistMusica.java
+++ backEnd/src/com/freetify/model/PlaylistMusica.java
@@ -1,12 +1,28 @@
 package com.freetify.model;
 
-import java.util.
+import java.time.LocalDateTime;
 
 public class PlaylistMusica {
-  
+
   private Musica musica;
   private Playlist playlist;
   private int posicao;
-  private 
+  private LocalDateTime dataAdicao;
 
+  public PlaylistMusica(Musica musica, Playlist playlist, int posicao, LocalDateTime dataAdicao){
+    this.musica = musica;
+    this.playlist = playlist;
+    this.posicao = posicao;
+    this.dataAdicao = dataAdicao;
+  }
+
+  public void setMusica(Musica musica){ this.musica = musica; }
+  public void setPlaylist(Playlist playlist){ this.playlist = playlist; }
+  public void setPosicao(int posicao){ this.posicao = posicao; }
+  public void setDataAdicao(LocalDateTime dataAdicao){ this.dataAdicao = dataAdicao; }
+
+  public Musica getMusica(){ return musica; }
+  public Playlist getPlaylist(){ return playlist; }
+  public int getPosicao(){ return posicao; }
+  public LocalDateTime getDataAdicao(){ return dataAdicao; }
 }
